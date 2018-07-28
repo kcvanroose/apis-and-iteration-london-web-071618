663d51c671891e535d1f6449bacac52c6b8bc62f
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-07-28 15:32:32 +0100

    finished lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,10 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+  character_data = character_hash["results"].find { |data| data["name"] == character }
+  film_urls = character_data["films"]
+  film_data = film_urls.collect { |data| JSON.parse(RestClient.get(data)) }
+  film_data
   
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
@@ -20,6 +24,9 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each.with_index(1) do |data, index|
+    puts "#{index} " + data['title']
+  end
 end
 
 def show_character_movies(character)
